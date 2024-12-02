59e2e16d63beb1020a3689d893c44ed93299b50e
phaze-the-dumb <phaze@duck.com>
2024-12-02 10:21:39 +0000

    allow any version of the game to connect

--- Titanium-Sharp/Packets/UDP/ServerData.cs
+++ Titanium-Sharp/Packets/UDP/ServerData.cs
@@ -13,12 +13,12 @@ public class ServerDataPacket : Packet
     
     _buf.PutInt(100); // Total Players
     _buf.PutInt(32); // Wave
-    _buf.PutInt(146); // Build Version
+    _buf.PutInt(-1); // Build Version
     
     _buf.PutString("[#a4b8fa]Titanium"); // Server Software / Version
     
     _buf.PutByte(0); // Game Mode
-    _buf.PutInt(1000); // Player Limit
+    _buf.PutInt(-1); // Player Limit
     
     _buf.PutString("Server Desc");
     _buf.PutString("Hell");
